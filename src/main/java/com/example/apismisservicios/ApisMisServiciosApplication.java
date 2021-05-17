19e8381bf9ea10ddeba51125a7389b42ce8e8789
Salva <svalladaresr@miumg.edu.gt>
2021-05-16 23:26:52 -0600

    Estructura inicial

---
+++ src/main/java/com/example/apismisservicios/ApisMisServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apismisservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisMisServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApisMisServiciosApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/apismisservicios/security/models/entities/Usuario.java
@@ -0,0 +1,68 @@
+package com.example.apismisservicios.security.models.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String username;
+    @Column(length = 60)
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "usuario_id"),
+    inverseJoinColumns = @JoinColumn(name = "rol_id"),
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "rol_id"})})
+    private List<Rol> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
