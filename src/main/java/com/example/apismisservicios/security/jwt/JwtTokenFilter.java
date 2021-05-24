39a95a100648ac04d2f9f18b2e655842950bf307
Salva <svalladaresr@miumg.edu.gt>
2021-05-24 12:26:41 -0600

    Clases de seguridad

--- src/main/java/com/example/apismisservicios/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/apismisservicios/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,53 @@
 package com.example.apismisservicios.security.jwt;
 
-public class JwtTokenFilter {
+import com.example.apismisservicios.security.services.UserDetailServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserDetailServiceImpl userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+
+            String token = getToken(request);
+            if(token != null && jwtProvider.validateToken(token)){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailService.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+        }catch (Exception e){
+            logger.error("Error en el método doFilterInternal");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer"))
+            return header.replace("Bearer ", "");
+        else
+            return  null;
+    }
 }
